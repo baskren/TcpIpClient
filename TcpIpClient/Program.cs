421b2bb1faeced01686ab4a40c3575cdf0fa5fe3
Ben Askren <ben@buildcalc.com>
2022-04-07 17:51:05 -0400

    REFACTOR : housecleaning

--- TcpIpClient/Program.cs
+++ TcpIpClient/Program.cs
@@ -22,92 +22,10 @@ public class SocketClient
 
         try
         {
-            /*
-            // Connect to a Remote server
-            // Get Host IP Address that is used to establish a connection
-            // In this case, we get one IP address of localhost that is IP : 127.0.0.1
-            // If a host has multiple addresses, you will get a list of addresses
-            IPHostEntry host = Dns.GetHostEntry("localhost");
-            //IPAddress ipAddress = host.AddressList[0];
-            IPAddress ipAddress = IPAddress.Parse("127.0.0.1");
-            IPEndPoint remoteEP = new IPEndPoint(ipAddress, 23);
-
-            // Create a TCP/IP  socket.
-            //Socket sender = new Socket(ipAddress.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
-            Socket sender = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
-            sender.SetSocketOption(SocketOptionLevel.IP, SocketOptionName.TypeOfService, 0x03);
-
-            // Connect the socket to the remote endpoint. Catch any errors.
-            try
-            {
-                // Connect to Remote EndPoint
-                sender.Connect(remoteEP);
-
-                Console.WriteLine($"Socket connected to {sender.RemoteEndPoint}");
-
-                // Encode the data string into a byte array.
-                
-                byte[] msg = Encoding.ASCII.GetBytes("ATI");
-                var msgx = new List<byte>(msg);
-                msgx.Add(0x0D);
-                msgx.Add(0x0A);
-                //msgx.Add(0);
-                msg = msgx.ToArray();
-
-                // Send the data through the socket.
-                int bytesSent = sender.Send(msg);
-
-                // Receive the response from the remote device.
-                int bytesRec = sender.Receive(bytes);
-                Console.WriteLine($"Echoed test = [{Encoding.ASCII.GetString(bytes, 0, bytesRec)}]\n");
-
-                // Release the socket.
-                sender.Shutdown(SocketShutdown.Both);
-                sender.Close();
- 
-            }
-            catch (ArgumentNullException ane)
-            {
-                Console.WriteLine("ArgumentNullException : {0}", ane.ToString());
-            }
-            catch (SocketException se)
-            {
-                Console.WriteLine("SocketException : {0}", se.ToString());
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine("Unexpected exception : {0}", e.ToString());
-            }
-            */
-
 
             var client = new TcpClient();
             client.Connect(new IPEndPoint(IPAddress.Loopback, 23));
             var stream = client.GetStream();
-
-            /*
-            byte[] msg = Encoding.ASCII.GetBytes("ATI");
-            var msgx = new List<byte>(msg);
-            msgx.Add(0x0D);
-            msgx.Add(0x0A);
-            //msgx.Add(0);
-            msg = msgx.ToArray();
-            stream.Write(msg, 0, msg.Length);
-            Console.WriteLine($"SENT: [{Encoding.ASCII.GetString(msg)}]");
-            
-            var data = new Byte[1024];
-            var responseData = string.Empty;
-
-            var byteCount = stream.Read(data, 0, data.Length);
-            while (byteCount > 0)
-            {
-                responseData = Encoding.ASCII.GetString(data);
-                //Console.WriteLine($"bytes recv: [{byteCount}][{responseData}]");
-                Console.WriteLine($"========= RECV START ========= \n\n {responseData}  \n\n =========== RECV END  ==========");
-                byteCount = 0;
-                byteCount = stream.Read(data, 0, data.Length);
-            }
-            */
 
             Task.Run(async () => await Listen(stream));
             Send(stream);
@@ -129,15 +47,8 @@ public class SocketClient
         while (byteCount > 0)
         {
             responseData = Encoding.ASCII.GetString(data);
-            //responseData = responseData.Replace("\r\n", "\n");
             responseData = responseData.Replace("\r", "\n");
-            //responseData = responseData.Replace("\n\n", "\n");
             Console.Write(responseData);
-            /*
-            foreach (var charx in responseData)
-                if (charx >= 10 && charx < 128)
-                Console.WriteLine($"[{charx}][{(int)charx}]");
-            */
             data = new byte[1024];
             byteCount = await stream.ReadAsync(data, 0, data.Length);
         }
